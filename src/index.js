d8bcb2856204756f35602b8bcb6c587af0b61574
adam-elamrani <adelamco1091@outlook.com>
2022-03-07 14:10:41 +0100

    Added dataBaseStart at index.js IIFE

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ const chalk = require("chalk");
 
 const serverStart = require("./server/serverStart");
 const app = require("./server/index");
+const dataBaseStart = require("./database");
 
 const port = process.env.PORT || 4000;
+const dbConnection = process.env.MONGODB_STRING;
 
 (async () => {
   try {
+    await dataBaseStart(dbConnection);
     await serverStart(port, app);
   } catch (error) {
     debug(chalk.redBright(error.message));
