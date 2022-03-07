57a200208e9df911881a0edfe2a9781d1ed0d749
adam-elamrani <adelamco1091@outlook.com>
2022-03-07 15:11:19 +0100

    Added errors middleware. Created notFound and general error functions

---
+++ src/server/middlewares/errors.js
@@ -0,0 +1,15 @@
+const chalk = require("chalk");
+const debug = require("debug")("Coinster:ErrorMiddleware:");
+
+const notFoundError = (req, res) => {
+  res.status(404).json({error: true, message: "Resource not found"}));
+};
+
+const generalError = (err, req, res, next) => {
+  debug(chalk.red(`Woops! ${err.message}`));
+  const errorCode = err.code ?? 500;
+  const errorMessage = err.code ? err.message : "General error";
+  res.status(errorCode).json({error: true, message: errorMessage});
+};
+
+module.exports = {notFoundError, generalError};
