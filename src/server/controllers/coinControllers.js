898681c48676313ec539428c904a754698ea405e
adam-elamrani <adelamco1091@outlook.com>
2022-03-13 10:02:15 +0100

    Add deleteCrypto controller

--- src/server/controllers/coinControllers.js
+++ src/server/controllers/coinControllers.js
@@ -8,4 +8,21 @@ const getCryptos = async (req, res) => {
   debug(chalk.bgYellow.black("Requested Crytos"));
 };
 
-module.exports = getCryptos;
+const deleteCrypto = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const crypto = await Crypto.findByIdAndDelete(id);
+    if (crypto) {
+      res.json(`Crypto ${crypto.name} with id ${crypto.id} deleted.`);
+    } else {
+      const error = new Error("Crypto not found");
+      debug(chalk.bgBlackBright.white(`Error: ${error.message}`));
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    debug(chalk.red(`Error: ${error.message}`));
+    next(error);
+  }
+};
+module.exports = { getCryptos, deleteCrypto };
