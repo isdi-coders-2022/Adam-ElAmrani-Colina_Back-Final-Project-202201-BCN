ce7fcb44005b2132758f25b2a6d68787b3f6aeae
adam-elamrani <adelamco1091@outlook.com>
2022-03-15 13:36:02 +0100

    Create method create new crypto

--- src/server/routes/cryptoRoutes.js
+++ src/server/routes/cryptoRoutes.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-const { getCryptos, deleteCrypto } = require("../controllers/coinControllers");
+const {
+  getCryptos,
+  deleteCrypto,
+  createCrypto,
+} = require("../controllers/coinControllers");
 
 const router = express.Router();
 
 router.get("/list", getCryptos);
 router.delete("/crypto/:id", deleteCrypto);
+router.post("/new-coin", createCrypto);
 
 module.exports = router;
