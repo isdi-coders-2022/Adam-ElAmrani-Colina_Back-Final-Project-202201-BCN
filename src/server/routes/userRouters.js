895261c2eab018095650388da75fc66d42c46581
adam-elamrani <adelamco1091@outlook.com>
2022-03-22 13:35:18 +0100

    Error controlled getUser controller

--- src/server/routes/userRouters.js
+++ src/server/routes/userRouters.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const { register, login } = require("../controllers/userControllers");
+const { register, login, getUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/profile", getUser);
 
 module.exports = router;
